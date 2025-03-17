4d823e7aa2db4b39aa14e329f9f6da59da3b099d
Nischal <nyyscal541556@gmail.com>
2025-03-17 17:18:44 +0545

    Custom Hook Added

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#030014",
+        secondary:"#151312",
+        accent:"#ab8bff",
+        light:{
+          100:"#d6c6ff",
+          200:"#a8b5db0",
+          300:"#9ca4ab"
+        },
+        dark:{
+          100:"#221f3d",
+          200:"#0f0d23"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
